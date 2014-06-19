a7623389246de438160b053fd794d8fec7e3480a
Scott Hillman <scotth@mediarain.com>
2014-06-19 05:46:53 -0600

    Close to done

--- file-watcher.go
+++ file-watcher.go
@@ -18,10 +18,16 @@ type FileMeta struct {
 
 func main() {
 	flag.Parse()
+	args := flag.Args()
+
+	if len(args) != 2 {
+		fmt.Printf("Command line usage: file-watcher <folder> <file-pattern>\n")
+		return
+	}
+
 	root := flag.Arg(0)
 	pattern := flag.Arg(1)
 
-	ticker := time.NewTicker(1 * time.Second)
 	var previousFiles, currentFiles, modifiedFiles map[string]FileMeta
 
 	previousFiles = getFilesMatchingPattern(pattern, root)
@@ -31,18 +37,28 @@ func main() {
 		fmt.Printf("Start values: %s %d\n", fileName, fileMeta.LineCount)
 	}
 
+	ticker := time.NewTicker(1 * time.Second)
 	for {
 		select {
 		case <-ticker.C:
 			// Get a current listing of the files and their mod times
 			currentFiles = getFilesMatchingPattern(pattern, root)
 
+			newFiles, deletedFiles := getNewAndDeletedFiles(previousFiles, currentFiles)
+
+			for newFile := range newFiles {
+				fmt.Printf("New file: %s\n", newFile)
+			}
+
+			for deletedFile := range deletedFiles {
+				fmt.Printf("Deleted file: %s\n", deletedFile)
+			}
+
 			// Get a list of the file names that have been modified based off the mod time
 			// Go through and get updated line counts for just the modified files
 			modifiedFiles = getModifiedFiles(previousFiles, currentFiles)
 			if len(modifiedFiles) > 0 {
 				countFilesLines(modifiedFiles)
-
 				// Loop through the modified files and list the line count change compared to the preivous check.
 				// Update the LineCount property in the currentFiles list as we go.
 				for fileName, fileMeta := range modifiedFiles {
@@ -50,8 +66,9 @@ func main() {
 					fmt.Printf("Last count: %d new count: %d\n", previousFileMeta.LineCount, fileMeta.LineCount)
 					currentFiles[fileName] = fileMeta
 				}
-				previousFiles = currentFiles
+
 			}
+			previousFiles = currentFiles
 		}
 	}
 }
@@ -83,7 +100,20 @@ func getModifiedFiles(previousFiles, currentFiles map[string]FileMeta) map[strin
 }
 
 func getNewAndDeletedFiles(previousFiles, currentFiles map[string]FileMeta) ([]string, []string) {
-	return nil
+	var newFiles, deletedFiles []string
+	for fileName, _ := range previousFiles {
+		if _, ok := currentFiles[fileName]; !ok {
+			deletedFiles = append(deletedFiles, fileName)
+		}
+	}
+
+	for fileName, _ := range currentFiles {
+		if _, ok := previousFiles[fileName]; !ok {
+			newFiles = append(newFiles, fileName)
+		}
+	}
+
+	return newFiles, deletedFiles
 }
 
 func countFilesLines(files map[string]FileMeta) {
